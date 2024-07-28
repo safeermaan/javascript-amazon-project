f840e571b063d82ec75b68c49d89b99c781481a6
safeermaan <safeermaan@gmail.com>
2024-07-29 03:53:07 +0500

    moved addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,18 @@
 export const cart = [];
+
+export function addToCart(productId) {
+    let matchingItem;
+      cart.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+          matchingItem = cartItem;
+        }
+      });
+      if (matchingItem) {
+        matchingItem.quanity += 1; 
+      } else {
+        cart.push({
+          productId: productId,
+          quanity: 1
+        });
+      }
+  }
