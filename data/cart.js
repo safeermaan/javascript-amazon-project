a946995d464d667f8afee0ed0b14cf594f08b676
safeermaan <safeermaan@gmail.com>
2024-08-01 04:14:49 +0500

    save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,7 @@
-export let cart = [
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+  cart =  [
   {
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quanity: 2
@@ -6,9 +9,12 @@ export let cart = [
   {
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     quanity: 1
-  }
+  }];
+}
 
-];
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
     let matchingItem;
@@ -25,6 +31,8 @@ export function addToCart(productId) {
           quanity: 1
         });
       }
+
+    saveToStorage();
   }
 
 export function removeFromCart(productId) {
@@ -36,4 +44,5 @@ export function removeFromCart(productId) {
       }
     });
     cart = newCart;
+    saveToStorage();
   }
